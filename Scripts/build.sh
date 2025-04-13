f8d71d9290405f4ee8c92149a90632610f60daf0
André Lademann <alademann@thinkport.digital>
2025-04-13 10:01:41 +0200

    ci(workflow): add build and release workflow for resume
    Introduce a GitHub Actions workflow to automate the build and
    release process for resume documents. This workflow triggers on
    tagged pushes and manual dispatch, builds the resume using Docker,
    and uploads PDF and EPUB artifacts. It then creates a GitHub release
    with these artifacts, enhancing the release process efficiency and
    ensuring consistent document availability.
    
    chore(workflow): simplify build-and-release workflow by removing EPUB support and redundant steps
    
    refactor(build.sh): remove generation of multiple PDF documents to streamline build process

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -98,39 +98,6 @@ done
 find Temp -type f -name '*.md"' -delete
 
 ################################################################################
-## Generate Multiple Documents
-################################################################################
-
-## Generate separate PDF files for each Markdown file in the content directory
-echo "\n✅\tGenerate PDF for each file"
-
-# Generate PDF files
-for file in Temp/*.md; do
-  # Check if $file is a file
-  if [ -f "$file" ]; then
-    # Get the filename without the extension
-    filename=$(basename -- "$file")
-    filename="${filename%.*}"
-    # Strip leading numbers and following dash from filename
-    filename=$(echo $filename | sed 's/^[0-9]-*//')
-
-    # Get the headline from the Markdown file
-    title=$(grep -m 1 '^# ' $file | sed 's/# //')
-
-    echo "👉\tBuild single PDF for \"${title}\""
-    docker run -v $PWD:/data ghcr.io/vergissberlin/pandoc-eisvogel-de ${file} \
-      -o Results/${RESUME_FILENAME}-${filename}.pdf \
-      --defaults Template/Config/defaults-pdf-single.yml \
-      --metadata-file Template/Config/metadata-pdf.yml \
-      -V title="${title}" \
-      -V author="${RESUME_AUTHOR}" \
-      -V subject="${RESUME_SUBJECT}" \
-      -V footer-center="v$document_git_tag" \
-      -V date="$document_date";
-  fi
-done
-
-################################################################################
 ## Generate PDF with combined content
 ################################################################################
 
